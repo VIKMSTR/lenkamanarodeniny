7edbf003d8a012cc0ec83f9e34057478e675dc4f
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2021-07-28 14:15:14 +0200

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Tailwind from './components/Tailwind.svelte';
+  
+	export let name;
+  </script>
+  
+  <Tailwind />
+<!-- <nav class="bg-blue-900 shadow-lg">
+	<div class="container mx-auto">
+	  <div class="sm:flex">
+		<a href="#" class="text-white text-3xl font-bold p-3">APP LOGO</a>
+		
+		<!-- Menus -->
+		<!-- <div class="ml-55 mt-4">
+		  <ul class="text-white sm:self-center text-xl">
+			<li class="sm:inline-block">
+			  <a href="#" class="p-3 hover:text-red-900">About</a>
+			</li>
+			<li class="sm:inline-block">
+			  <a href="#" class="p-3 hover:text-green-900">Services</a>
+			</li>
+			<li class="sm:inline-block">
+			  <a href="#" class="p-3 hover:text-red-900">Blog</a>
+			</li>
+			<li class="sm:inline-block">
+			  <a href="#" class="p-3 hover:text-red-900">Contact</a>
+			</li>
+		  </ul>
+		</div>
+  
+	  </div>
+	</div>
+  </nav> --> 
+<body class="bg-gray-800">
+<main class="container mx-auto">
+	<p class="text-center text-8xl align-middle text-green-300">COMING SOON</p>
+</main>
+</body>
