a52e848ead6f1abed4de3db99341058887a2bed2
Viktor Krejcir <viktor.krejcir@fibiworks.com>
2021-07-28 14:46:13 +0200

    updated frontpage

--- src/App.svelte
+++ src/App.svelte
@@ -31,8 +31,32 @@
 	  </div>
 	</div>
   </nav> --> 
-<body class="bg-gray-800">
-<main class="container mx-auto">
-	<p class="text-center text-8xl align-middle text-green-300">COMING SOON</p>
-</main>
-</body>
+
+  <div class="flex h-screen justify-center items-center">
+    <div class="text-center"> <!-- ⬅️ THIS DIV WILL BE CENTERED -->
+		<p class="text-center text-8xl align-middle text-green-300 uppercase">už se to blíží </p>
+    </div>
+  </div>
+  <div class="flex h-screen justify-center items-center">
+    <div class="text-center"> <!-- ⬅️ THIS DIV WILL BE CENTERED -->
+		<p class="text-center text-8xl align-middle text-yellow-300 uppercase">v září to rozzáří</p>
+    </div>
+  </div>
+  <div class="flex h-screen justify-center items-center">
+    <div class="text-center"> <!-- ⬅️ THIS DIV WILL BE CENTERED -->
+		<p class="text-center text-8xl align-middle text-red-300 uppercase">tak prostě počkej, no</p>
+    </div>
+  </div>
+  <div class="flex h-screen justify-center items-center">
+    <div class="text-center"> <!-- ⬅️ THIS DIV WILL BE CENTERED -->
+		<p class="text-center text-8xl align-middle text-blue-300 uppercase ">*chjo*</p>
+    </div>
+  </div>
+  <div class="flex h-screen justify-center items-center">
+    <div class="text-center"> <!-- ⬅️ THIS DIV WILL BE CENTERED -->
+		<p class="text-center text-7xl align-middle text-orange-300 uppercase">ok, tady je zatím na zkrácení volné chvíle</p>
+		<div class="flex justify-center content-center">
+		<iframe class="flex justify-center content-center" width="640" height="480" src="https://www.youtube.com/embed/p3l7fgvrEKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+    </div>
+  </div>
